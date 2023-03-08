e8c365d83a4c8eef0c266616f5ec0b68ab0c5291
AzureAD\JelenaMatecic <jelena.matecic@syntio.net>
2023-03-08 18:20:34 +0100

    inital commit

---
+++ app/src/main/java/com/example/kunaify/viewModel/ExchangeViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.kunaify.viewModel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ExchangeViewModel : ViewModel() {
+    // TODO
+}
